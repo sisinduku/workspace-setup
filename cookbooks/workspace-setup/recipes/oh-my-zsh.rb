02d427f500d4a3696b5bcb8e5b15bd19b1f76c08
sisinduku <saptanto.sindu@gmail.com>
2018-04-27 16:54:09 +0700

    Add spec to chef to install spaceship prompt

--- cookbooks/workspace-setup/recipes/oh-my-zsh.rb
+++ cookbooks/workspace-setup/recipes/oh-my-zsh.rb
@@ -6,5 +6,15 @@
 
 execute 'Installing oh-my-zsh' do
   command 'sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
-  not_if File.directory?(File.expand_path('~/.oh-my-zsh'))
+  not_if { File.directory?(File.expand_path('~/.oh-my-zsh')) }
+end
+
+execute 'Installing spaceship prompt' do
+  command 'git clone https://github.com/denysdovhan/spaceship-prompt.git "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt"'
+  not_if { File.directory?(File.expand_path('~/.oh-my-zsh/custom/themes/spaceship-prompt')) }
+end
+
+execute 'Create symlink of spaceship prompt' do
+  command 'ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"'
+  not_if { File.exist?(File.expand_path('~/.oh-my-zsh/custom/themes/spaceship.zsh-theme')) }
 end
--- test/integration/default/oh-my-zsh_test.rb
+++ test/integration/default/oh-my-zsh_test.rb
@@ -1,3 +1,11 @@
 describe directory('/home/kitchen/.oh-my-zsh') do
-  it { should exist  }
+  it { should exist }
+end
+
+describe directory('/home/kitchen/.oh-my-zsh/custom/themes/spaceship-prompt') do
+  it { should exist }
+end
+
+describe file('/home/kitchen/.oh-my-zsh/custom/themes/spaceship.zsh-theme') do
+  it { should exist }
 end
