53aaf4f6b0ec801a7660c2f6c48a05cad5b54c92
sisinduku <saptanto.sindu@gmail.com>
2018-05-03 17:54:19 +0700

    Add rbenv dependency

--- Berksfile
+++ Berksfile
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
+cookbook 'ruby_rbenv', '~> 2.0.7'
 cookbook 'workspace-setup', path: './cookbooks/workspace-setup'
--- cookbooks/workspace-setup/recipes/oh-my-zsh.rb
+++ cookbooks/workspace-setup/recipes/oh-my-zsh.rb
@@ -3,7 +3,6 @@
 # Recipe:: oh-my-zsh
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-
 execute 'installing oh-my-zsh' do
   command 'sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"'
   not_if { File.directory?(File.expand_path('~/.oh-my-zsh')) }
@@ -33,3 +32,4 @@ execute 'create symlink of spaceship prompt' do
   command 'ln -s "$HOME/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$HOME/.oh-my-zsh/custom/themes/spaceship.zsh-theme"'
   not_if { File.exist?(File.expand_path('~/.oh-my-zsh/custom/themes/spaceship.zsh-theme')) }
 end
+
